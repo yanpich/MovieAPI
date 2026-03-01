87efc9631c3b9299441d0e6c37f3f6782dff9a2c
yanpich <yanpich154@gmail.com>
2026-03-01 21:10:55 +0700

    complete project

--- src/main/java/com/movieflix/movieapi/controller/MovieController.java
+++ src/main/java/com/movieflix/movieapi/controller/MovieController.java
@@ -3,15 +3,16 @@ package com.movieflix.movieapi.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.movieflix.movieapi.dto.MovieDto;
+import com.movieflix.movieapi.dto.MoviePageResponse;
 import com.movieflix.movieapi.service.MovieService;
-import jakarta.persistence.TypedQuery;
+import com.movieflix.movieapi.utils.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.management.Query;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -55,7 +56,22 @@ public class MovieController {
     public ResponseEntity<MovieDto> getAllMoviesByIdHandler(@PathVariable Integer movieId) {
         return ResponseEntity.ok(movieService.getMovie(movieId));
     }
-
+    @GetMapping("/allMoviesPage")
+    public ResponseEntity<MoviePageResponse> getMoviesWithPagination(
+            @RequestParam(defaultValue = AppConstants.PAGE_NUMBER,required = false) Integer pageNumber,
+            @RequestParam(defaultValue = AppConstants.PAGE_SIZE,required = false) Integer pageSize
+    ){
+        return ResponseEntity.ok(movieService.getAllMoviesWithPagination(pageNumber, pageSize));
+    }
+    @GetMapping("/allMoviesPageSort")
+    public ResponseEntity<MoviePageResponse> getMoviesWithPaginationSorting(
+            @RequestParam(defaultValue = AppConstants.PAGE_NUMBER,required = false) Integer pageNumber,
+            @RequestParam(defaultValue = AppConstants.PAGE_SIZE,required = false) Integer pageSize,
+            @RequestParam(defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+            @RequestParam(defaultValue = AppConstants.SORT_DIR, required = false) String sortDir
+    ){
+        return ResponseEntity.ok(movieService.getAllMoviesWithPaginationAndSorting(pageNumber, pageSize, sortBy, sortDir));
+    }
 
 
 
